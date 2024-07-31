0804718510296f22d9a2559681506c1f62da0620
Dennis Kimathi <kimathidennis38@gmail.com>
2024-07-31 12:15:43 +0300

    enums

--- struct_enums/src/main.rs
+++ struct_enums/src/main.rs
@@ -18,6 +18,20 @@ struct Rectangle {
 
 struct Empty; // unit struct
 
+enum Message {
+    Quit, 
+    Move { x: i32, y: i32 },
+    Write (String),
+    ChangeColor(i32, i32, i32),
+
+}
+
+enum Animal {
+    Dog(String),
+    Cat(String),
+    Bird(String),
+}
+
 
 fn main() {
 
@@ -27,6 +41,8 @@ fn main() {
     };
 
 
+
+
     println!("My name is {:?} and I am {:?} years old", chizaa.name, chizaa.age);
 
     let p1 = Point { x: 1.0, y: 1.0 };
@@ -34,33 +50,34 @@ fn main() {
 
     println!("The distance between p1 and p2 is: {}", distance(p1, p2));
 
-    impl Empty {
-        fn greet (&self) {
-            println!("Hello");
-
-        }
-    }
+  
 
     let empty_instance = Empty;
     empty_instance.greet();
 
-    impl Rectangle {
-        fn area(&self) -> f64 {
-            self.width * self.height
-        }
-
-        fn perimeter(&self) -> f64 {
-            2.0 * (self.width + self.height)
-        }
-
-     
-    }
+   
 
 
     let rect1 = Rectangle { width: 30.0, height: 50.0 };
 
     println!("The area of the rectangle is: {}", rect1.area());
     println!("The perimeter of the rectangle is: {}", rect1.perimeter());
+
+    let msg = Message::Write(String::from("hello"));
+    process_message(msg);
+
+    let my_pet = Animal::Cat("Fluf".to_string());
+
+    if let Animal::Cat(name) = my_pet {
+        println!("My cat's name is: {}", name);
+    } else {
+        println!("My pet is not a cat.");
+    }
+
+
+   
+    let msg = Message::Write(String::from("Hello, Rust!"));
+    msg.call();
 
 
 }
@@ -70,5 +87,53 @@ fn distance(p1: Point, p2: Point) -> f64 {
     let y_diff = p1.y - p2.y;
 
     (x_diff * x_diff + y_diff * y_diff).sqrt()
+
+}
+
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Move { x, y } => println!("Move to x: {}, y: {}", x, y),
+            Message::Write(s) => println!("Write: {}", s),
+            Message::ChangeColor(r, g, b) => println!("Change color to R: {}, G: {}, B: {}", r, g, b),
+        }
+    }
+}
+
+impl Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * (self.width + self.height)
+    }
+
+ 
+}
+
+impl Empty {
+    fn greet (&self) {
+        println!("Hello");
+
+    }
+}
+
+fn process_message (msg: Message) {
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        },
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        },
+        Message::Write (text) => {
+            println!("Text message: {}", text);
+        },
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b);
+        },
+    }
 
 }
